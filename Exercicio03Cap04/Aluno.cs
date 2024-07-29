f865f12214dcb8218135f39080b536d85db91d53
Matheus Gonçalves De Britto <94484304+omatheusbritto@users.noreply.github.com>
2024-07-28 23:43:22 -0300

    Aluno.cs

--- Exercicio03Cap04/Aluno.cs
+++ Exercicio03Cap04/Aluno.cs
@@ -1,13 +1,15 @@
-﻿using System;
+using System;
 
 namespace Exercicio03Cap04
 {
     class Aluno
     {
-        public double Nota01, Nota02,  Nota03;
+        public double Nota01, Nota02,  Nota03, Faltante;
         public double NotaDoAluno()
         {
             double TotalDasNotas = Nota01 + Nota02 + Nota03;
+            Faltante = 60 - TotalDasNotas;
+            
             return TotalDasNotas;
         }
         public override string ToString()
@@ -15,8 +17,12 @@ namespace Exercicio03Cap04
         {
             if (NotaDoAluno() < 60)
             {
+
                return "Aluno Reprovado \nSua nota foi de "
-                + NotaDoAluno();
+                + NotaDoAluno()
+                + "\nFaltam "
+                + Faltante
+                +" para ser aprovado";
             }
             else
             {
